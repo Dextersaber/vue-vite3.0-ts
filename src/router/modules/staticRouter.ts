5ec1f97996ebc4bd033601cc548bc08b1e186b7f
huanggenming <genming.huang@lerke.cn>
2023-04-13 11:32:30 +0800

    fix:tada:更新

--- src/router/modules/staticRouter.ts
+++ src/router/modules/staticRouter.ts
@@ -48,7 +48,7 @@ export const staticRouter: RouteRecordRaw[] = [
         name: "DataLabeler1",
         component: () => import("@/layouts/index.vue"),
         meta: {
-          title: "测试22",
+          title: "测试2",
         },
       },
     ],
